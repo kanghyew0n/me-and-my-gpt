202a92bbbc9fc89ce96cd4cceb4aaad02214d345
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-09-12 04:02:19 +0900

    💄 Design: 변수명 추천 디자인 변경

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import Header from "./Header";
 
-const Layout = ({ children, onSubmit }) => {
+const Layout = ({ children }) => {
   return (
     <LayoutContainer>
-      <Header />
       <BodyInner>{children}</BodyInner>
     </LayoutContainer>
   );
@@ -12,7 +11,7 @@ const Layout = ({ children, onSubmit }) => {
 
 const LayoutContainer = styled.div`
   width: 100%;
-  padding-top: 20px; // 헤더의 블러 부분 제외하고 위로 보이지 않게 하기위해 설정
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
